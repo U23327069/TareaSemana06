bfbd2f7fd0b03258abe00870b7c346c14af0675d
miguel.coveñas <miguel.coveas@PYT08.smartglobal.pe>
2024-09-22 18:15:52 -0500

    Actualización desarrollo de la semana 06.

---
+++ src/Ejercicio1_Semana06.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio1_Semana06 {
+    public static void main(String[] args) {
+        int contador = 1; // inicialización del contador
+        
+        // bucle while que se ejecuta mientras el contador sea menor o igual a 10
+        while (contador <= 5) {
+            System.out.println("Numero: " + contador);
+            contador++; // incremento del contador
+        }
+    }
+}
