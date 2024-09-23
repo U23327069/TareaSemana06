fb425f740dfc8e3e39dcb931d93ff7069c33d418
miguel.coveñas <miguel.coveas@PYT08.smartglobal.pe>
2024-09-22 19:07:47 -0500

    Actualización desarrollo de la semana 06.

--- src/Ejercicio1_Semana06.java
+++ src/Ejercicio1_Semana06.java
@@ -1,13 +1,29 @@
 import java.util.Scanner;
 public class Ejercicio1_Semana06 {
     public static void main(String[] args) {
-        //Iniciar el contador de numeros
-        int contador = 1;
-        //Contador sea menor o igual a 15
-        while (contador <= 15) {
-            System.out.println("Numero: " + contador);
-            contador++;
-            // Fin de Programa
+        Scanner scanner = new Scanner(System.in);
+        //Declaramos las variables
+        int numero;
+        //almacenar el número ingresado por el usuario
+        int factorial = 1;
+        //iniciar el factorial en 1, ya que 0! = 1
+        int contador;
+        //aplicamos el bucle y entrada de datos
+        System.out.print("Ingrese un número entero positivo: ");
+        numero = scanner.nextInt();        
+        //El número tiene que ser positivo
+        if (numero < 0) {
+            System.out.println("Por favor, ingrese un número positivo.");
+        } else {
+            contador = numero;
+            //Calcular el factorial
+            while (contador > 0) {
+                factorial *= contador;
+                //Se multiplica el factorial por el contador
+                contador--;
+            }
+            // Salida de datos
+            System.out.println("El factorial de " + numero + " es: " + factorial);
         }
     }
 }
